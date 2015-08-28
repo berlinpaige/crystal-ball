bf91b05778398234ef39f5467c41a36e4c713061
Berlin Sohn <berlinsohn@gmail.com>
2015-08-28 14:17:51 -0700

    remove some html, convert to javascript object for better ux

--- crystalBall.js
+++ crystalBall.js
@@ -1,4 +1,28 @@
 $(document).ready(function(){
+
+var hiddenFortunes = {
+      0: 'When do you want to?',
+      1: "Don't even think about it!",
+      2: 'Do you really think I know the answer?',
+      3: "I don't think so!",
+      4: 'Maybe you should ask Siri',
+      5: 'Yes! And be proud of it',
+      6: 'When pigs fly!',
+      7: 'Go for a run, then come back and ask.',
+      8: 'Do you really place your confidence in me?',
+      9: "You won't be happy with my answer.",
+      10: "Of course!",
+      11: 'Absolutely not!',
+      12: 'Did you ask your mom?',
+      13: 'Are you sure?',
+      14: 'Maybe you should wait a couple years',
+      15: 'Is that what you really want?',
+      16: 'How long have you been waiting to ask that?',   
+      17: 'Of course!! It was meant to be'
+}
+
+
+
   $('p').hide();
   $('.input').keypress(function(event){
     event.preventDefault();
@@ -8,8 +32,8 @@ $(document).ready(function(){
     event.preventDefault();
     $('h3').hide();
     $('h4').hide();
-    var findRandomFortune = Math.floor(Math.random() * 17); 
-    var info = document.getElementById(findRandomFortune).innerHTML;
-  $(this).append('<h3>' + info + '</h3>'); 
+    var findRandomFortune = Math.floor(Math.random() * 17);
+    var messageToShow = hiddenFortunes[findRandomFortune];
+    $(this).append('<h3>' + messageToShow + '</h3>'); 
   });
 });
