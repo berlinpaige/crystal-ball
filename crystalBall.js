f20398e5ab6eb2b839970a133082363a1cf1e6f8
Berlin Sohn <berlinsohn@gmail.com>
2015-08-28 14:20:10 -0700

    clean up spaces

--- crystalBall.js
+++ crystalBall.js
@@ -1,28 +1,24 @@
 $(document).ready(function(){
-
 var hiddenFortunes = {
-      0: 'When do you want to?',
-      1: "Don't even think about it!",
-      2: 'Do you really think I know the answer?',
-      3: "I don't think so!",
-      4: 'Maybe you should ask Siri',
-      5: 'Yes! And be proud of it',
-      6: 'When pigs fly!',
-      7: 'Go for a run, then come back and ask.',
-      8: 'Do you really place your confidence in me?',
-      9: "You won't be happy with my answer.",
-      10: "Of course!",
-      11: 'Absolutely not!',
-      12: 'Did you ask your mom?',
-      13: 'Are you sure?',
-      14: 'Maybe you should wait a couple years',
-      15: 'Is that what you really want?',
-      16: 'How long have you been waiting to ask that?',   
-      17: 'Of course!! It was meant to be'
+  0: 'When do you want to?',
+  1: "Don't even think about it!",
+  2: 'Do you really think I know the answer?',
+  3: "I don't think so!",
+  4: 'Maybe you should ask Siri',
+  5: 'Yes! And be proud of it',
+  6: 'When pigs fly!',
+  7: 'Go for a run, then come back and ask.',
+  8: 'Do you really place your confidence in me?',
+  9: "You won't be happy with my answer.",
+  10: "Of course!",
+  11: 'Absolutely not!',
+  12: 'Did you ask your mom?',
+  13: 'Are you sure?',
+  14: 'Maybe you should wait a couple years',
+  15: 'Is that what you really want?',
+  16: 'How long have you been waiting to ask that?',   
+  17: 'Of course!! It was meant to be'
 }
-
-
-
   $('p').hide();
   $('.input').keypress(function(event){
     event.preventDefault();
